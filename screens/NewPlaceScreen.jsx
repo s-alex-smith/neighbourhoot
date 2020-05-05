1633c66d70c9bf41503a617058aaa607179ea6a9
jakedheath123 <jakedheath@gmail.com>
2020-05-05 13:00:18 +0100

    add feature to pass user location to NewPlaceScreen

--- screens/NewPlaceScreen.jsx
+++ screens/NewPlaceScreen.jsx
@@ -6,74 +6,84 @@ import * as Permissions from "expo-permissions";
 
 import { getBirdsToPopulateMap } from "../apiRequest/apiRequests";
 
-const NewPlaceScreen = () => {
-	const [databaseLocations, setDatabaseLocation] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+const NewPlaceScreen = ({ userLocation }) => {
+  console.log(userLocation);
+  const [databaseLocations, setDatabaseLocation] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // const [selectedLocation, setSelectedLocation] = useState();
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 51.3805849,
+    longitude: -0.1479596,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  });
 
-	const getBirds = () => {
-		getBirdsToPopulateMap().then((response) => {
-			setDatabaseLocation(response);
-			// setIsLoading(false);
-		});
-	};
+  // if (userLocation) {
+  //   setMapRegion({
+  //     latitude: userLocation.lat,
+  //     longitude: userLocation.lng,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421
+  //   });
+  // }
 
-	useEffect(() => {
-		getBirds();
-	}, []);
+  const getBirds = () => {
+    getBirdsToPopulateMap().then(response => {
+      setDatabaseLocation(response);
+      // setIsLoading(false);
+    });
+  };
 
-	const [selectedLocation, setSelectedLocation] = useState();
-	const mapRegion = {
-		latitude: 51.3805849,
-		longitude: -0.1479596,
-		latitudeDelta: 0.0922,
-		longitudeDelta: 0.0421,
-	};
+  useEffect(() => {
+    getBirds();
+  }, []);
 
-	const selectLocationHandler = (event) => {
-		setSelectedLocation({
-			lat: event.nativeEvent.coordinate.latitude,
-			lng: event.nativeEvent.coordinate.longitude,
-		});
-	};
+  const selectLocationHandler = event => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude
+    });
+  };
 
-	let markerCoordinates;
+  // let markerCoordinates;
 
-	if (selectedLocation) {
-		markerCoordinates = {
-			latitude: selectedLocation.lat,
-			longitude: selectedLocation.lng,
-		};
-	}
+  // if (selectedLocation) {
+  //   markerCoordinates = {
+  //     latitude: selectedLocation.lat,
+  //     longitude: selectedLocation.lng
+  //   };
+  // }
 
-	return (
-		<MapView
-			style={styles.map}
-			region={mapRegion}
-			// onPress={selectLocationHandler}>
-			// {markerCoordinates && (
-			// 	<Marker title='Picked Location' coordinate={markerCoordinates}></Marker>
-			// )}
-		>
-			{databaseLocations.map((location, index) => {
-				const locationSpec = {
-					latitude: location.lat,
-					longitude: location.lng,
-				};
-				return (
-					<Marker
-						key={index}
-						title={location.comName}
-						coordinate={locationSpec}></Marker>
-				);
-			})}
-		</MapView>
-	);
+  return (
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      // onPress={selectLocationHandler}>
+      // {markerCoordinates && (
+      // 	<Marker title='Picked Location' coordinate={markerCoordinates}></Marker>
+      // )}
+    >
+      {databaseLocations.map((location, index) => {
+        const locationSpec = {
+          latitude: location.lat,
+          longitude: location.lng
+        };
+        return (
+          <Marker
+            key={index}
+            title={location.comName}
+            coordinate={locationSpec}
+          ></Marker>
+        );
+      })}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
-	map: {
-		flex: 1,
-	},
+  map: {
+    flex: 1
+  }
 });
 
 export default NewPlaceScreen;
