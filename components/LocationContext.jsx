f93c45b73b3a676ded7d519778581f479eee31f3
JasminBower <jasmin.ibower@gmail.com>
2020-05-13 15:50:17 +0100

    changed user id

--- components/LocationContext.jsx
+++ components/LocationContext.jsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 
 const LocationContext = React.createContext();
 
-const LocationProvider = props => {
-  const [context, setContext] = useState({
-    location: "?lat=57.355333&lon=-2.507852",
-    user_id: "00631bc1-75c9-4cd6-ad90-cdd7c0560829",
-    lat: 57.355333,
-    lon: -2.507852
-  });
-  return (
-    <LocationContext.Provider value={[context, setContext]}>
-      {props.children}
-    </LocationContext.Provider>
-  );
+const LocationProvider = (props) => {
+	const [context, setContext] = useState({
+		location: "?lat=57.355333&lon=-2.507852",
+		user_id: "448ae798-190a-4352-a793-7013ae2721ac",
+		lat: 57.355333,
+		lon: -2.507852,
+	});
+	return (
+		<LocationContext.Provider value={[context, setContext]}>
+			{props.children}
+		</LocationContext.Provider>
+	);
 };
 
 export { LocationContext, LocationProvider };
